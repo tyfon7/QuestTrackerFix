ad0afa3807c10261b015eeee2e1d750cb560a18f
Ben Hamlin <bhamlin@microsoft.com>
2017-05-29 15:00:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This addon simply restores the old behavior of clicking a quest in the quest tracker. Before 7.2, this would open your quest details. For some reason in 7.2, Blizzard broke this and now it opens your map or your quest+map, depending on which one was opened more recently. 
+
+With this addon, it will always open your quest log.
+
+This is done by simply restoring the old code, which I got from https://github.com/tekkub/wow-ui-source
---
+++ main.lua
@@ -0,0 +1,25 @@
+-- This is Blizzard's old implementation, via github.com/tekkub/wow-ui-source
+local orig_OnBlockHeaderClick = QUEST_TRACKER_MODULE.OnBlockHeaderClick;
+function QUEST_TRACKER_MODULE:OnBlockHeaderClick(block, mouseButton)
+    if ( ChatEdit_TryInsertQuestLinkForQuestID(block.id) ) then
+        return;
+    end
+
+    if ( mouseButton ~= "RightButton" ) then
+        CloseDropDownMenus();
+        if ( IsModifiedClick("QUESTWATCHTOGGLE") ) then
+            QuestObjectiveTracker_UntrackQuest(nil, block.id);
+        else
+            local questLogIndex = GetQuestLogIndexByID(block.id);
+            if ( IsQuestComplete(block.id) and GetQuestLogIsAutoComplete(questLogIndex) ) then
+                AutoQuestPopupTracker_RemovePopUp(block.id);
+                ShowQuestComplete(questLogIndex);
+            else
+                QuestLogPopupDetailFrame_Show(questLogIndex);
+            end
+        end
+        return;
+    else
+        ObjectiveTracker_ToggleDropDown(block, QuestObjectiveTracker_OnOpenDropDown);
+    end
+end
